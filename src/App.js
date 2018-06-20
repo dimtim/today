7f4fa858c695a3943dc59eac24ad0c7c2ea2ed0c
Dmitry.Timakin <Dmitry.Timakin@vineyard.ru>
2018-06-20 20:29:54 +0300

    Create react app

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  startDate = new Date('2018-06-14T17:28:01.893Z');
+
+  addDays(days) {
+    let day = new Date();
+    day.setDate(day.getDate() + days);
+    return day >= this.startDate ? day.toLocaleDateString() : '';
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>{this.addDays(-1)}</div>
+        <div>{this.addDays(-2)}</div>
+        <div>{this.addDays(-5)}</div>
+        <div>{this.addDays(-10)}</div>
+        <div>{this.addDays(-20)}</div>
+        <div>{this.addDays(-60)}</div>
       </div>
     );
   }
